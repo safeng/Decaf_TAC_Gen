3d8bd38d206f8ce5582c7a4094c071063f4b3e95
Hongjiu Zhang <nocte107@nocte107.name>
2014-03-28 12:22:07 -0400

    Add a check for main function
      * ast_stmt.cc (Program::Emit): Add a check for main function.
    
    Signed-off-by: Hongjiu Zhang <zhanghj@umich.edu>

--- ast_decl.h
+++ ast_decl.h
@@ -13,72 +13,72 @@ class FnDecl;
 
 class Decl : public Node
 {
-  protected:
-    Identifier *id;
+    protected:
+        Identifier *id;
 
-  public:
-    Decl(Identifier *name);
-    friend std::ostream& operator<<(std::ostream& out, Decl *d) { return out << d->id; }
-    Identifier *GetId() { return id; }
-    const char *GetName() { return id->GetName(); }
+    public:
+        Decl(Identifier *name);
+        friend std::ostream& operator<<(std::ostream& out, Decl *d) { return out << d->id; }
+        Identifier *GetId() { return id; }
+        const char *GetName() { return id->GetName(); }
 
-    virtual bool ConflictsWithPrevious(Decl *prev);
+        virtual bool ConflictsWithPrevious(Decl *prev);
 
-    virtual bool IsVarDecl() { return false; } // jdz: could use typeid/dynamic_cast for these
-    virtual bool IsClassDecl() { return false; }
-    virtual bool IsFnDecl() { return false; }
-    virtual bool IsMethodDecl() { return false; }
-    virtual bool IsIvarDecl() { return false; }
+        virtual bool IsVarDecl() { return false; } // jdz: could use typeid/dynamic_cast for these
+        virtual bool IsClassDecl() { return false; }
+        virtual bool IsFnDecl() { return false; }
+        virtual bool IsMethodDecl() { return false; }
+        virtual bool IsIvarDecl() { return false; }
 };
 
 class VarDecl : public Decl
 {
-  protected:
-    Type *type;
+    protected:
+        Type *type;
 
-  public:
-    VarDecl(Identifier *name, Type *type);
-    void Check();
-    Type *GetDeclaredType() { return type; }
-    bool IsVarDecl() { return true; }
-    bool IsIvarDecl();
+    public:
+        VarDecl(Identifier *name, Type *type);
+        void Check();
+        Type *GetDeclaredType() { return type; }
+        bool IsVarDecl() { return true; }
+        bool IsIvarDecl();
 };
 
 class ClassDecl : public Decl
 {
-  protected:
-    List<Decl*> *members;
-    NamedType *extends;
-    NamedType *cType;
+    protected:
+        List<Decl*> *members;
+        NamedType *extends;
+        NamedType *cType;
 
-  public:
-    ClassDecl(Identifier *name, NamedType *extends, List<Decl*> *members);
-    void Check();
-    bool IsClassDecl() { return true; }
-    Scope *PrepareScope();
-    bool IsCompatibleWith(Type *type);
-    Type *GetDeclaredType() { return cType; } //  used by "this"
-    const char *GetClassName() { return id->GetName(); }
+    public:
+        ClassDecl(Identifier *name, NamedType *extends, List<Decl*> *members);
+        void Check();
+        bool IsClassDecl() { return true; }
+        Scope *PrepareScope();
+        bool IsCompatibleWith(Type *type);
+        Type *GetDeclaredType() { return cType; } //  used by "this"
+        const char *GetClassName() { return id->GetName(); }
 };
 
 class FnDecl : public Decl
 {
-  protected:
-    List<VarDecl*> *formals;
-    Type *returnType;
-    Stmt *body;
+    protected:
+        List<VarDecl*> *formals;
+        Type *returnType;
+        Stmt *body;
 
-  public:
-    FnDecl(Identifier *name, Type *returnType, List<VarDecl*> *formals);
-    void SetFunctionBody(Stmt *b);
-    void Check();
-    void CheckPrototype();
-    bool IsFnDecl() { return true; }
-    bool IsMethodDecl();
-    bool ConflictsWithPrevious(Decl *prev);
-    bool MatchesPrototype(FnDecl *other);
-    Type *GetReturnType() {return returnType; }
-    List<VarDecl*> *GetFormals() { return formals; }
+    public:
+        FnDecl(Identifier *name, Type *returnType, List<VarDecl*> *formals);
+        void SetFunctionBody(Stmt *b);
+        void Check();
+        void CheckPrototype();
+        bool IsFnDecl() { return true; }
+        bool IsMethodDecl();
+        bool ConflictsWithPrevious(Decl *prev);
+        bool MatchesPrototype(FnDecl *other);
+        Type *GetReturnType() {return returnType; }
+        List<VarDecl*> *GetFormals() { return formals; }
 };
 
 #endif
